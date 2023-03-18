9eb758b3486aec72eecbf6bf388d0f0a3a891cf8
moorts <moritz.gutfleisch@gmx.de>
2023-03-18 16:02:59 +0100

    dwm autostart

---
+++ dwm/.dwm/autostart.sh
@@ -0,0 +1,2 @@
+setxkbmap us,de -option grp:alt_shift_toggle -option ctrl:nocaps
+nm-applet
---
+++ dwm/.dwm/autostart_blocking.sh
@@ -0,0 +1,1 @@
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x180 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off && feh --bg-fill ~/.dwm/haskWallpaperGreyscale3440x1440.png
